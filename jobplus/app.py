0299752cae1c156f34aaedebba8f54634ace03cf
jiker1 <329021121@qq.com>
2019-06-27 17:10:21 +0800

    Add files via upload

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_migrate import Migrate
-from flask_login import LoginManager
 from jobplus.config import configs
 from jobplus.models import db, User
-from jobplus.handlers import blueprints
+from flask_login import LoginManager
 
-def register_extension(app):
+
+def register_extensions(app):
     db.init_app(app)
     Migrate(app, db)
     login_manager = LoginManager()
@@ -14,34 +14,24 @@ def register_extension(app):
     @login_manager.user_loader
     def user_loader(id):
         return User.query.get(id)
+
     login_manager.login_view = 'front.login'
 
 
-def register_blueprints(app: Flask):
-    for bp in blueprints:
-        app.register_blueprint(bp)
+def register_blueprints(app):
+    from .handlers import front, admin, user, company, job
+    app.register_blueprint(front)
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+    app.register_blueprint(company)
+    app.register_blueprint(job)
 
-
-def register_error_handler(app: Flask):
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return render_template('error/404.html', 404)
-
-    @app.errorhandler(500)
-    def server_error(error):
-        return render_template('error/500.html', 500)
 
 def create_app(config):
     app = Flask(__name__)
+    app.config.from_object(configs.get(config))
 
-    if isinstance(config, dict):
-        app.config.update(config)
-    else:
-        app.config.from_object(configs.get(config, None))
-
-    register_extension(app)
+    register_extensions(app)
     register_blueprints(app)
-    register_error_handler(app)
-    return app
 
+    return app
--- jobplus/config.py
+++ jobplus/config.py
@@ -1,13 +1,12 @@
-class BaseConfig:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '\xd6+@+\xc6\x03\xaa\r>o>c\xb3\x1f2z\x9c;\x8f\xbaXoo\xc9'
+class BaseConfig(object):
+    SECRET_KEY = 'very secret key'
     INDEX_PER_PAGE = 9
     ADMIN_PER_PAGE = 15
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/jobplus?charset=utf8'
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
 
 
 class ProductionConfig(BaseConfig):
